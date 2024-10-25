b1ae53c1f774d0bb779a40fb9c7320e12eef978e
JimenaMartin <jimena.martin@masmovil.com>
2024-10-25 20:31:57 +0200

    more playing around

--- src/Experience.js
+++ src/Experience.js
@@ -1,54 +1,63 @@
-import { useFrame, extend, useThree } from "@react-three/fiber";
+import { extend } from "@react-three/fiber";
+import {
+  OrbitControls,
+  TransformControls,
+  PivotControls,
+  Html,
+  Text,
+  Float,
+  MeshReflectorMaterial
+} from "@react-three/drei";
 import { useRef } from "react";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import CustomObject from "./CustomObject";
 
-extend({ OrbitControls})
+extend({ OrbitControls });
 
-export  function Experience() {
+export function Experience() {
+  const cubeRef = useRef();
+  const sphereRef = useRef()
 
-  const cubeRef = useRef()
-  const groupRef = useRef()
-
-  const {camera, gl} = useThree()
-
-  useFrame((state, delta) => {
-    cubeRef.current.rotation.y += delta;
-    // const angle = state.clock.elapsedTime
-    // state.camera.position.x = Math.sin(angle) * 8
-    // state.camera.position.z = Math.cos(angle) * 8
-    // state.camera.lookAt(0, 0, 0)
-  })
-    
-    return (
-      <>
-        <orbitControls args={[camera, gl.domElement]} />
-
-        <directionalLight position={[ 1, 2, 3]} intensity={4.5}  />
-        <ambientLight intensity={1.5} />
-
-        <group ref={groupRef}>
-          <mesh position-x={-2}>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-          </mesh>
-          <mesh
-            ref={cubeRef}
-            scale={1.5}
-            position-x={2}
-            rotation-y={Math.PI * 0.25}
-          >
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-          </mesh>
-        </group>
-        <mesh scale={10} rotation-x={-Math.PI * 0.5} position-y={-1}>
-          <planeGeometry args={[5, 5]} />
-          <meshStandardMaterial color="greenyellow" />
+  return (
+    <>
+      <OrbitControls makeDefault />
+      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={4}
+        scale={120}
+        fixed
+      >
+        <mesh ref={sphereRef} position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+          <Html distanceFactor={8} center position={[1, 1, 0]} wrapperClass="label" occlude={[sphereRef, cubeRef]}>
+            That's a sphere
+          </Html>
         </mesh>
+      </PivotControls>
 
+      <mesh
+        ref={cubeRef}
+        position-x={2}
+        scale={1.5}
+        rotation-y={Math.PI * 0.25}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+      <TransformControls object={cubeRef} mode="translate" />
 
-        <CustomObject />
-      </>
-    );
+      <mesh scale={10} rotation-x={-Math.PI * 0.5} position-y={-1}>
+        <planeGeometry args={[5, 5]} />
+        {/* <meshStandardMaterial color="greenyellow" /> */}
+        <MeshReflectorMaterial resolution={512} blur={[1000, 1000]} mixBlur={1} mirror={0.75} color="greenyellow" />
+      </mesh>
+      <Float>
+        <Text speed={3} color="salmon" position-y={2} maxWidth={2} textAlign="center">
+          I LOVE R3F
+        </Text>
+      </Float>
+    </>
+  );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,26 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import { Experience } from './Experience'
-import * as THREE from 'three'
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./Experience";
+import * as THREE from "three";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-    <>
-        <Canvas
-            dpr={[1,2]}
-            gl={{ antialias: true, toneMapping: THREE.ACESFilmicToneMapping }}
-            camera={{
-                fov: 45, near: 0.1, far: 200, position: [3,2,6]
-            }}
-        >
-            <mesh>
-                <Experience />
-            </mesh>
-        </Canvas>
-    </>
-)
+  <>
+    <Canvas
+      dpr={[1, 2]}
+      gl={{ antialias: true, toneMapping: THREE.ACESFilmicToneMapping }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [3, 2, 10],
+      }}
+    >
+      <mesh>
+        <Experience />
+      </mesh>
+    </Canvas>
+  </>
+);
