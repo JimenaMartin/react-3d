00d36a5ed768a7e06b5e66bf84f1605f20ceeaa0
JimenaMartin <jimena.martin@masmovil.com>
2024-10-25 12:29:50 +0200

    first lesson

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,19 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import { Experience } from './Experience'
+import * as THREE from 'three'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <>
-        <Canvas>
+        <Canvas
+            dpr={[1,2]}
+            gl={{ antialias: true, toneMapping: THREE.ACESFilmicToneMapping }}
+            camera={{
+                fov: 45, near: 0.1, far: 200, position: [3,2,6]
+            }}
+        >
             <mesh>
                 <Experience />
             </mesh>
